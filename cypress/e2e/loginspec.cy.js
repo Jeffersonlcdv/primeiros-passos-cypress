42216bcc904e7336dcaa22c99983ca509f08ebfa
unknown <jeffersonluis_cv@hotmail.com>
2025-05-26 21:24:08 -0300

    Added Const UserData in Fixtures

--- cypress/e2e/loginspec.cy.js
+++ cypress/e2e/loginspec.cy.js
@@ -1,3 +1,5 @@
+import userData from '../fixtures/userData.json'
+
 describe('Orange Tests', () => {
 
   const selectorsList = {
@@ -5,21 +7,23 @@ describe('Orange Tests', () => {
     passwordFiel: "[name='password']",
     loginbutton: "[type='submit']",
     sectionTitleTopBar: ".oxd-topbar-header-breadcrumb-module",
+    dashboardGrip:".orangehrm-dashboard-grid",
     wrongCredentialAlert: "[role='alert']"
   }
 
+
   it('Login - success', () => {
     cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
-    cy.get(selectorsList.usernameField).type('Admin')
-    cy.get(selectorsList.passwordFiel).type('admin123')
+    cy.get(selectorsList.usernameField).type(userData.userSucess.username)
+    cy.get(selectorsList.passwordFiel).type(userData.userSucess.password)
     cy.get(selectorsList.loginbutton).click()
     cy.location('pathname').should('equal', '/web/index.php/dashboard/index') //utilizado para encontrar algo na url para garantir que acessou apos click
-    cy.get(selectorsList.sectionTitleTopBar).contains('Dashboard')
+    cy.get(selectorsList.dashboardGrip)
     })
     it('Login - fail', () => {
       cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
-      cy.get(selectorsList.usernameField).type('Admin1')
-      cy.get(selectorsList.passwordFiel).type('admin123')
+      cy.get(selectorsList.usernameField).type(userData.userFail.username)
+      cy.get(selectorsList.passwordFiel).type(userData.userFail.password)
       cy.get(selectorsList.loginbutton).click()
       cy.get(selectorsList.wrongCredentialAlert)
       })
