0afb769fafc46378165e50ea1b8566e93ef35db8
unknown <jeffersonluis_cv@hotmail.com>
2025-06-02 19:18:09 -0300

    Added baseUrl to cypress.config

--- cypress/e2e/user.spec.cy.js
+++ cypress/e2e/user.spec.cy.js
@@ -8,17 +8,19 @@ describe('Orange Tests', () => {
     loginbutton: "[type='submit']",
     sectionTitleTopBar: ".oxd-topbar-header-breadcrumb-module",
     dashboardGrip:".orangehrm-dashboard-grid",
-    wrongCredentialAlert: "[role='alert']"
+    wrongCredentialAlert: "[role='alert']",
+    myInfoButton:"[href='/web/index.php/pim/viewMyDetails']"
   }
 
 
-  it('Login - success', () => {
+  it.only('User Info Update- success', () => {
     cy.visit('/auth/login')
     cy.get(selectorsList.usernameField).type(userData.userSucess.username)
     cy.get(selectorsList.passwordFiel).type(userData.userSucess.password)
     cy.get(selectorsList.loginbutton).click()
     cy.location('pathname').should('equal', '/web/index.php/dashboard/index') //utilizado para encontrar algo na url para garantir que acessou apos click
     cy.get(selectorsList.dashboardGrip)
+    cy.get(selectorsList.myInfoButton).click()
     })
     it('Login - fail', () => {
       cy.visit('/auth/login')
