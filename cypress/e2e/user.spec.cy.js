5fe0062ad8861094fcc3ea587e1193d68768dfd5
unknown <jeffersonluis_cv@hotmail.com>
2025-06-03 19:34:55 -0300

    Usr update scenario added

--- cypress/e2e/user.spec.cy.js
+++ cypress/e2e/user.spec.cy.js
@@ -4,23 +4,46 @@ describe('Orange Tests', () => {
 
   const selectorsList = {
     usernameField: "[name='username']",
-    passwordFiel: "[name='password']",
+    passwordField: "[name='password']",
     loginbutton: "[type='submit']",
     sectionTitleTopBar: ".oxd-topbar-header-breadcrumb-module",
     dashboardGrip:".orangehrm-dashboard-grid",
     wrongCredentialAlert: "[role='alert']",
-    myInfoButton:"[href='/web/index.php/pim/viewMyDetails']"
+    myInfoButton:"[href='/web/index.php/pim/viewMyDetails']",
+    firstNameField: "[name=firstName]",
+    lastNameField: "[name=lastName]",
+    middleNameField:"[name='middleName']",
+    genericField: ".oxd-input--active",
+    dateField: "[placeholder='yyyy-dd-mm']",
+    otherId: "(':nth-child(3) > :nth-child(1) > :nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input')",
+    closeButton:".--close",
+    saveButton:"[type='submit']"
+    
+
+
   }
 
 
   it.only('User Info Update- success', () => {
     cy.visit('/auth/login')
     cy.get(selectorsList.usernameField).type(userData.userSucess.username)
-    cy.get(selectorsList.passwordFiel).type(userData.userSucess.password)
+    cy.get(selectorsList.passwordField).type(userData.userSucess.password)
     cy.get(selectorsList.loginbutton).click()
     cy.location('pathname').should('equal', '/web/index.php/dashboard/index') //utilizado para encontrar algo na url para garantir que acessou apos click
     cy.get(selectorsList.dashboardGrip)
     cy.get(selectorsList.myInfoButton).click()
+    cy.get(selectorsList.genericField).eq(4).clear().type('teste123 ')
+    cy.get(':nth-child(3) > :nth-child(1) > :nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input').clear().type('Othe id teste')
+    cy.get(selectorsList.genericField).eq(5).clear().type('123456')
+    cy.get(selectorsList.dateField).eq(0).clear().type('2025-03-06')
+    cy.get(selectorsList.closeButton).click()
+    cy.get(selectorsList.dateField).eq(1).clear().type('2025-03-06')
+    cy.get(selectorsList.closeButton).click()
+    cy.get(selectorsList.saveButton).eq(0).click()
+    cy.get('body').should('contain', 'Successfully Updated')
+    cy.get('.oxd-toast-close')
+    
+    
     })
     it('Login - fail', () => {
       cy.visit('/auth/login')
@@ -28,5 +51,6 @@ describe('Orange Tests', () => {
       cy.get(selectorsList.passwordFiel).type(userData.userFail.password)
       cy.get(selectorsList.loginbutton).click()
       cy.get(selectorsList.wrongCredentialAlert)
+        
       })
 })
